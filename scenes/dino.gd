f5c9f6088fe0543ee256b3daf82e71cdf6fb8da1
allefrodrigo <dev.allefschmidt@gmail.com>
2024-05-09 23:20:27 -0300

    :rocket: launch first demo

---
+++ scenes/dino.gd
@@ -0,0 +1,30 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -1800.0
+const GRAVITY : int = 4200
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	velocity.y += GRAVITY * delta
+	if is_on_floor():
+		if not get_parent().game_running:
+			$AnimatedSprite2D.play("idle")
+		else:
+			$RunCol.disabled = false
+			if Input.is_action_pressed("ui_accept"):
+				velocity.y = JUMP_VELOCITY
+				$JumpSound.play()
+			elif Input.is_action_pressed("ui_down"):
+				$AnimatedSprite2D.play("duck")
+				$RunCol.disabled = true
+			else:
+				$AnimatedSprite2D.play("run")
+	else:
+		$AnimatedSprite2D.play("jump")
+		
+	move_and_slide()
